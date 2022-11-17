585c4d0cb04a137dcde1cc8ddd957a69c40e7966
user <yusufbekhasanov5@gmail.com>
2022-11-17 16:12:35 +0500

    BlackQuize

---
+++ app/src/main/java/com/example/blackquziegame/models/Flag.kt
@@ -0,0 +1,17 @@
+package com.example.blackquziegame.models
+
+import android.media.Image
+
+class Flag {
+    var name:String? = null
+    var image:Int? = null
+
+ constructor(name: String?,image: Int?){
+     this.name = name
+     this.image = image
+ }
+
+
+
+
+}
